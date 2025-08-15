d52d30aa9611c88dfdcfaa37f3591106cff3830e
Reksa Maulana <161690316+Ecaks25@users.noreply.github.com>
2025-08-15 17:56:15 +0700

    Rename stock pages to Terima TTPB

--- resources/views/blower/stock.blade.php
+++ resources/views/blower/stock.blade.php
@@ -1,9 +1,9 @@
-@section('title', __('Blower Stock'))
-<x-layouts.app :title="__('Blower Stock')">
+@section('title', __('Blower Terima TTPB'))
+<x-layouts.app :title="__('Blower Terima TTPB')">
 
 <div class="card">
     <div class="card-header">
-        <h5 class="card-title mb-0">{{ __('Stock') }}</h5>
+        <h5 class="card-title mb-0">{{ __('Terima TTPB') }}</h5>
     </div>
     <div class="card-body">
 @include('partials.month-filter')
